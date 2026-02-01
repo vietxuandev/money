1c228acc3220e99fb4cc150b04299c1359c6fda2
Jayden Nguyen <xuan.nguyen.tpv@one-line.com>
2026-02-01 20:51:41 +0700

    delay issue

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./i18n/config";
 import App from "./App.tsx";
-import { initMobileFixes } from "./lib/mobile-fixes";
-
-// Initialize mobile touch fixes
-initMobileFixes();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
